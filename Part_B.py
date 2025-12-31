5e6a01fc42f52ada9121757e149d108ac2b9d550
CharlesWel <158081720+CharlesWel@users.noreply.github.com>
2025-12-31 01:25:28 +0000

    Update Part_B.py

--- Part_B.py
+++ Part_B.py
@@ -1,14 +1,19 @@
-import maths 
+import math 
 import pandas as pd
 import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
 
-cvsData = pd.read_cvs("heart_failure_clinical_records_dataset.csv")
+cvsData = pd.read_csv("heart_failure_clinical_records_dataset.csv")
 
 inputCols = ["age","anaemia","creatinine_phosphokinase","diabetes","ejection_fraction","high_blood_pressure","platelets","serum_creatinine","serum_sodium","sex","smoking","time"] 
 answerCol = "DEATH_EVENT"
 
 xData = cvsData[inputCols].values
 yData = cvsData[answerCol].values
+
+mins = xData.min()
+maxs = xData.max()
+xData = (xData - mins) / (maxs - mins)
 
 xTrain, xTemp, yTrain, yTemp = train_test_split(xData, yData, test_size=0.30, random_state=42)
 xVal, xTest, yVal, yTest = train_test_split(xTemp, yTemp, test_size=0.50, random_state=42)
@@ -35,24 +40,25 @@ def predictProb(x, y, a):
     
   return sigmoid(temp)
 
+def predictLable(x, y, a):
+  if predictProb(x, y, a) >= 0.5:
+    return 1
+    
+  else:
+    return 0
+
+
 def accuracy(x, y, a, b):
   correct = 0
   sizeA = len(a)
   
   for i in range(sizeA):
-    if predictProb(x, y, a[i]) == b[i]:
+    if predictLable(x, y, a[i]) == b[i]:
       correct += 1
       
   temp = correct / sizeA
   
   return temp
-
-def predictLable(x, y, a):
-  if preictProb(x, y, a) >= 0.5:
-    return 1
-    
-  else:
-    return 0
 
 def meanSquareError(x, y, a, b):
   totalSE = 0
@@ -62,7 +68,7 @@ def meanSquareError(x, y, a, b):
     prob = predictProb(x, y, a[i])
     error = prob - b[i]
     totalSE += error * error
-  return s / temp
+  return totalSE / temp
 
 def gradientDescent(a, b, learningRate, passes):
   weightVector = [0] * 12
@@ -79,7 +85,7 @@ def gradientDescent(a, b, learningRate, passes):
       for l in range(12):
         gradientLoss[l] += error * a[j][l]
         
-      bias += error
+      gradientLossBias += error
       
     meanGrad = 1 / len(a)
     
@@ -88,8 +94,18 @@ def gradientDescent(a, b, learningRate, passes):
       
     bias -= learningRate * (gradientLossBias * meanGrad)
   
-  return weightVector, bias    
+  return weightVector, bias 
 
+print("Enter 1 to")
+print("Enter 2 to")
+#choice = int(input(""))
+choice = 1
 
+if choice == 1:
+  weightvector, bias = gradientDescent(XTrain, YTrain, 0.1, 100)
 
-  
+  trainingAccuracy = accuracy(weightvector, bias, XTrain,  YTrain)
+  validationAccuracy = accuracy(weightvector, bias, XVal, YVal)
+
+  print("Training classification rate:", trainingAccuracy)
+  print("Validation classification rate", validationAccuracy)
